7bd06455aa11faf49f6f4c3d3473b3ec59450cf7
andrewswint <andrewswint1976@gmail.com>
2019-11-15 15:40:06 -0600

    cards

--- src/index.js
+++ src/index.js
@@ -12,6 +12,17 @@ const {getMovie, getMovies, postMovie, patchMovie, deleteMovie} = require('./api
  */
 // const {getMovies} = require('./api.js');
 
+//---------------------generate cards----------------------
+function makeCard(title, rating){
+  let card;
+  card = "";
+  card += `<div class="card">`;
+  // card += `<img src="..." class="card-img-top" alt="...">`;
+  card += `<div class="card-body">`;
+  card += `<p class="card-text">${title}, ${rating}</p>`;
+  card += `</div> </div>`;
+  document.getElementById("card-container").innerHTML += card;
+}
 
 
 //function that grabs and displays movies/movie info
@@ -19,18 +30,21 @@ getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
+    let newTitle = `${title}`;
+    let newRating = `${rating}`;
+    makeCard(newTitle, newRating);
   });
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
 
-getMovie(1)
-    .then(movie => {
-      console.log("Making a request to a single movie");
-      console.log(`${movie.title} by ${movie.rating}`);
-    })
-    .catch(() => console.log('The important thing is you tried...'));
+// getMovie(1)
+//     .then(movie => {
+//       console.log("Making a request to a single movie");
+//       console.log(`${movie.title} by ${movie.rating}`);
+//     })
+//     .catch(() => console.log('The important thing is you tried...'));
 
 //  postMovie({
 //   "title": "Movie title",
@@ -45,25 +59,25 @@ getMovie(1)
 //   console.log(error);
 // });
 
-patchMovie({
-  "title": "title",
-  "rating": "5"
-}, 3).then(getMovies).then((movies) => {
-  console.log('Here are all the movies:');
-  movies.forEach(({title, rating}) => {
-    console.log(`${title} ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
-
-deleteMovie(4).then(getMovies).then((movies) => {
-  console.log('Here are all the books:');
-  movies.forEach(({title, rating}) => {
-    console.log(`${title} ${rating}`);
-  });
-}).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.');
-  console.log(error);
-});
+// patchMovie({
+//   "title": "title",
+//   "rating": "5"
+// }, 3).then(getMovies).then((movies) => {
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating}) => {
+//     console.log(`${title} ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.');
+//   console.log(error);
+// });
+//
+// deleteMovie(4).then(getMovies).then((movies) => {
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating}) => {
+//     console.log(`${title} ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.');
+//   console.log(error);
+// });
