845a2cb953a3010da3b81a3993f6837f989f0dab
andrewswint <andrewswint1976@gmail.com>
2019-11-15 14:49:50 -0600

    functionality

--- src/api.js
+++ src/api.js
@@ -2,5 +2,35 @@ module.exports = {
   getMovies: () => {
     return fetch('/api/movies')
       .then(response => response.json());
+  },
+  getMovie : (id) => {
+    return fetch(`/api/movies/${id}`)
+        .then(resp => resp.json());
+  },
+  postMovie : (movie) => {
+    return fetch('api/movies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movie),
+    })
+  },
+  patchMovie : (movie, id) => {
+    return fetch(`api/movies/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(movie),
+    })
+  },
+  deleteMovie : (id) => {
+    return fetch(`api/movies/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
   }
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@
 import {sayHello} from './hello.js';
 sayHello('World');
 
-import {getMovies} from './api.js';
+// import {getMovies} from './api.js';
+const {getMovie, getMovies, postMovie, patchMovie, deleteMovie} = require('./api.js');
 
 /**
  * require style imports
@@ -21,5 +22,48 @@ getMovies().then((movies) => {
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
+  console.log(error);
+});
+
+getMovie(1)
+    .then(movie => {
+      console.log("Making a request to a single movie");
+      console.log(`${movie.title} by ${movie.rating}`);
+    })
+    .catch(() => console.log('The important thing is you tried...'));
+
+//  postMovie({
+//   "title": "Movie title",
+//   "rating": "1"
+// }).then(getMovies).then((movies) => {
+//   console.log('Here are all the movies:');
+//   movies.forEach(({title, rating}) => {
+//     console.log(`${title} ${rating}`);
+//   });
+// }).catch((error) => {
+//   alert('Oh no! Something went wrong.\nCheck the console for details.');
+//   console.log(error);
+// });
+
+patchMovie({
+  "title": "title",
+  "rating": "5"
+}, 3).then(getMovies).then((movies) => {
+  console.log('Here are all the movies:');
+  movies.forEach(({title, rating}) => {
+    console.log(`${title} ${rating}`);
+  });
+}).catch((error) => {
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
+  console.log(error);
+});
+
+deleteMovie(4).then(getMovies).then((movies) => {
+  console.log('Here are all the books:');
+  movies.forEach(({title, rating}) => {
+    console.log(`${title} ${rating}`);
+  });
+}).catch((error) => {
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
